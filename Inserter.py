5330fa6e4a7dbd696451feddf24ad08b48871d69
Azero 77 <zaggar.azero.m@gmail.com>
2025-01-16 17:17:09 +0300

    initial

---
+++ Inserter.py
@@ -0,0 +1,20 @@
+from pymongo import MongoClient
+import json
+
+# Connect to MongoDB
+client = MongoClient("mongodb://localhost:27017/")  # Adjust the connection string if needed
+db = client["ExamQuestionsDatabase"]
+collection = db["Exams"]
+
+# Load the JSON file
+with open(r"E:\QuizApp\test.json",encoding = "utf-8") as file:
+    json_content = json.load(file)
+
+# Insert the JSON file content as a value to a specific key
+document = {
+    "Name": "Equillibrum",
+    "Questions": json_content
+}
+
+collection.insert_one(document)
+print("Data inserted successfully!")
---
+++ QuizAppAPI/QuizAppAPI/Controllers/ExamsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QuizAppAPI.Controllers
+{
+    /// <summary>
+    /// API to return the available exams to take
+    /// </summary>
+    public class ExamsController : Controller
+    {
+        
+    }
+}
