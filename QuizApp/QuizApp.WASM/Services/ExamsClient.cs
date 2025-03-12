680e11c1500b5db1ad16f000ead8d1cccc36d00a
Azero 77 <zaggar.azero.m@gmail.com>
2025-03-12 22:30:01 +0300

    Added Wasm Client

---
+++ QuizApp/QuizApp.WASM/Services/ExamsClient.cs
@@ -0,0 +1,17 @@
+﻿using QuizApp.Models;
+using System.Net.Http.Json;
+
+namespace QuizApp.BlazorWASM.Services
+{
+    public class ExamsClient
+    {
+        private readonly HttpClient _client;
+
+        public ExamsClient(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public IAsyncEnumerable<Exam?> GetExamsAsync() => _client.GetFromJsonAsAsyncEnumerable<Exam>("Exams") ?? throw new InvalidDataException();
+    }
+}
