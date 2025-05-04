42e8c14f10e92545573ea4e9c7bb23a9b125058a
Azero77 <zaggar.azero.m@gmail.com>
2025-05-04 10:16:30 +0300

    configured client identity

---
+++ QuizApp/QuizApp.Identity/Data/IdentityConstants.cs
@@ -0,0 +1,13 @@
+﻿namespace QuizApp.Identity.Data
+{
+    public static class ApplicationConstants
+    {
+        public static class Roles
+        {
+            public static string Admin = "Admin";
+            public static string User = "User";
+        }
+    }
+
+    
+}
